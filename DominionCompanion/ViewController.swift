c67ac2484ba4da9760969adefcc0b0c50922e596
Harris <harrisborawski@gmail.com>
2019-06-02 15:25:04 -0700

    Initial Commit

---
+++ DominionCompanion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DominionCompanion
+//
+//  Created by Harris Borawski on 6/2/19.
+//  Copyright © 2019 Harris Borawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
