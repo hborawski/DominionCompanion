9e50cecfd8043b3cd01ac87958ff29306eef37e0
Harris <harrisborawski@gmail.com>
2019-12-02 20:21:17 -0800

    remove unused code

--- DominionCompanion/Utilities/Utilities.swift
+++ DominionCompanion/Utilities/Utilities.swift
@@ -17,42 +17,5 @@ class Utilities {
     public static func priceSort(card1: Card, card2: Card) -> Bool {
         return card1.cost <= card2.cost
     }
-
-    public static func combos<T>(elements: ArraySlice<T>, k: Int) -> [[T]] {
-        if k == 0 {
-            return [[]]
-        }
-        
-        guard let first = elements.first else {
-            return []
-        }
-        
-        let head = [first]
-        let subcombos = combos(elements: elements, k: k - 1)
-        var ret = subcombos.map { head + $0 }
-        ret += combos(elements: elements.dropFirst(), k: k)
-        
-        return ret
-    }
-    
-    public static func combos<T>(elements: Array<T>, k: Int) -> [[T]] {
-        return combos(elements: ArraySlice(elements), k: k)
-    }
 }
 
-extension UIStackView {
-    
-    func removeAllArrangedSubviews() {
-        
-        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
-            self.removeArrangedSubview(subview)
-            return allSubviews + [subview]
-        }
-        
-        // Deactivate all constraints
-        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
-        
-        // Remove the views from self
-        removedSubviews.forEach({ $0.removeFromSuperview() })
-    }
-}
