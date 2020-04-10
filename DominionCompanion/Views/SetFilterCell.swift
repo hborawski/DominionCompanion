260c765245e679319ff629a68764973ec64076c5
Harris <harrisborawski@gmail.com>
2020-04-10 11:59:28 -0700

    move process to background thread to prevent lag

--- DominionCompanion/Views/SetFilterCell.swift
+++ DominionCompanion/Views/SetFilterCell.swift
@@ -14,24 +14,36 @@ class SetFilterCell : UITableViewCell {
     @IBOutlet var cardCountLabel: UILabel!    
     @IBOutlet weak var ruleStack: UIStackView!
     @IBOutlet weak var cardIcon: UIImageView!
-    
+    var rule: SetRule?
     func setData(rule: SetRule) {
         self.cardOperationLabel.text = rule.operation.rawValue
         self.cardCountLabel.text = "\(rule.value)"
-        if rule.satisfaction(RuleEngine.shared.cardData) < 1.0 {
-            self.cardCountLabel.textColor = .red
-        } else {
-            if #available(iOS 13, *) {
-                self.cardCountLabel.textColor = .label
-            } else {
-                self.cardCountLabel.textColor = .black
-            }
-        }
+        
         ruleStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
         rule.cardRules.forEach { rule in
             let label = UILabel()
             label.text = "\(rule.property.rawValue) \(rule.operation.rawValue) \(rule.comparisonValue)"
             ruleStack.addArrangedSubview(label)
+        }
+        setLabelColor()
+    }
+    
+    func setLabelColor() {
+        DispatchQueue.global(qos: .background).async {
+            guard let rule = self.rule else { return }
+            if !rule.satisfiable(RuleEngine.shared.cardData) {
+                DispatchQueue.main.async {
+                    self.cardCountLabel.textColor = .red
+                }
+            } else {
+                DispatchQueue.main.async {
+                    if #available(iOS 13, *) {
+                        self.cardCountLabel.textColor = .label
+                    } else {
+                        self.cardCountLabel.textColor = .black
+                    }
+                }
+            }
         }
     }
 }
